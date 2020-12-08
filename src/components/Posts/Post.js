e4bb12ab31ce171881406f32421c2adedbc18bda
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-12-08 12:46:44 -0700

    renders amount of likes on the page

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import Comments from '../Comments/Comments';
-import LikeSection from './LikeSection';
-import PostHeader from './PostHeader';
+import React from "react";
+import Comments from "../Comments/Comments";
+import LikeSection from "./LikeSection";
+import PostHeader from "./PostHeader";
 
-const Post = props => {
+const Post = (props) => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { post, likePost, id } = props;
 
   return (
-    <div className='post-border'>
+    <div className="post-border">
       <PostHeader
+        key={id}
         username={post.username}
         thumbnailUrl={post.thumbnailUrl}
       />
-      <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
-        />
+      <div className="post-image-wrapper">
+        <img alt="post thumbnail" className="post-image" src={post.imageUrl} />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={likePost} post={post}/>
+      <LikeSection key={id} likePost={likePost} post={post} />
       {/* Comments also wants its props! */}
-      <Comments comments={post.comments}/>
+      <Comments key={id} comments={post.comments} />
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { likePost, posts, id } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {posts.map((post) => {
-        return <Post likePost={likePost} post={post} />
+        return <Post key={id} likePost={likePost} post={post} />;
       })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
